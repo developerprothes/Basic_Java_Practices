cf2979156eb019dd36ed7d5b69f37feed24a15bd
prothesbarai <aspprothesbarai1619@gmail.com>
2024-11-29 12:45:12 +0600

    day 1

---
+++ basicjava/BasicJava.java
@@ -0,0 +1,8 @@
+package com.mycompany.basicjava;
+public class BasicJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Prothes Barai");
+    }
+}
